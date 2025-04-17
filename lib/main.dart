871910b049fef8e83e32c069071e64e41aa7b741
Muhammad-Musadiq <bscs23148@itu.edu.pk>
2025-04-17 15:29:50 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'MultiGame',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
